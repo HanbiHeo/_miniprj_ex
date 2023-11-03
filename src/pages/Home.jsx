0d7bb95cc1de550a9e459e71df0482dc6da5e915
HanbiHeo <hhb0365@gmail.com>
2023-11-03 09:10:16 +0900

    11 02

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,41 @@
 //홈화면
+//화면이동을 위한 훅 useNavigate
+import { useNavigate } from "react-router-dom";
+
+import { ButtonContainer, TransBtn } from "../component/ButtonContaitner";
+import imgBottom from "../images/nedbank_s.png";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const onClickBtn = (num) => {
+    switch (num) {
+      case 1:
+        navigate("/memberlist");
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        navigate("/setting");
+        break;
+      default:
+    }
+  };
+
   return (
     <>
-      <p>여기는 홈화면 입니다</p>
+      <div>
+        <ButtonContainer>
+          <TransBtn onClick={() => onClickBtn(1)}>회원리스트</TransBtn>
+          <TransBtn onClick={() => onClickBtn(2)}>뉴스보기</TransBtn>
+          <TransBtn onClick={() => onClickBtn(3)}>사진 업로드</TransBtn>
+          <TransBtn onClick={() => onClickBtn(4)}>테마 변경</TransBtn>
+        </ButtonContainer>
+        <div className="bdlogo">
+          <img src={imgBottom} alt="buttonimg" />
+        </div>
+      </div>
     </>
   );
 };
--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,1 +1,32 @@
 //설정페이지
+import { ButtonContainer, TransBtn } from "../component/ButtonContaitner";
+import { useContext } from "react";
+import { UserContext } from "../context/UserStore";
+
+const Setting = () => {
+  const context = useContext(UserContext);
+  const { setColor } = context;
+
+  return (
+    <>
+      <ButtonContainer>
+        <TransBtn>테마설정</TransBtn>
+      </ButtonContainer>
+      <ButtonContainer>
+        <TransBtn color="orange" onClick={() => setColor("orange")}>
+          오렌지
+        </TransBtn>
+        <TransBtn color="green" onClick={() => setColor("green")}>
+          그린
+        </TransBtn>
+        <TransBtn color="darkgrey" onClick={() => setColor("darkgrey")}>
+          그레이
+        </TransBtn>
+        <TransBtn color="royalblue" onClick={() => setColor("royalblue")}>
+          블루
+        </TransBtn>
+      </ButtonContainer>
+    </>
+  );
+};
+export default Setting;
